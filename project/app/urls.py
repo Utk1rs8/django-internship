b26552a4c73eb27a4cd636b484d13cbf1bda07b0
Utk1rs8 <utkarshchurariya19@gmail.com>
2024-10-14 12:57:22 +0530

    diff url

--- project/app/urls.py
+++ project/app/urls.py
@@ -1,6 +1,10 @@
 from .views import *
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register(r'',Bikesviewset, basename='user')
-urlpatterns = router.urls
+router.register(r'slider',Sliderviewset)
+router.register(r'bikes',Bikesviewset)
+urlpatterns = [
+    path('', include(router.urls)),
+]
